c6f625b045870a552f74d391b7b5afc5f8e0725f
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2023-02-09 22:46:12 +0530

    dailog update

--- src/component/account/LoginDailog.jsx
+++ src/component/account/LoginDailog.jsx
@@ -1,13 +1,22 @@
 import { Dialog } from '@mui/material'
 import React from 'react'
 
+const dailogStyle={
+    height:"96%",
+    marginTop:"12%",
+    width:"60%",
+    maxWidth:"100%",
+    maxHeight:"100%",
+    boxShadow:"none",
+    overFlow:"none"
 
+}
 const LoginDailog = () => {
   return (
     <div>
         <Dialog
         open={true}
-        //PaperProps={}
+        PaperProps={{sx:dailogStyle}}
         >
             hello
 
