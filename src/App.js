7965ed31ddc5065886951609a0cb65bca55030fb
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2023-02-15 01:07:06 +0530

    google outh complate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Messanger from './component/Messanger';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 function App() {
+  const clientId="381162290677-ari757054b1ijbo7v3ui5hv8eh1gp5fj.apps.googleusercontent.com"
   return (
-    <div>
+    <GoogleOAuthProvider clientId={clientId}>
       <Messanger/>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
--- src/component/account/LoginDailog.jsx
+++ src/component/account/LoginDailog.jsx
@@ -1,30 +1,36 @@
-import { Dialog, Box, Typography, ListItem, List,styled } from "@mui/material";
+import { Dialog, Box, Typography, ListItem, List, styled } from "@mui/material";
 import React from "react";
-import {qrCodeImage} from "../../Data/data"
+import { qrCodeImage } from "../../Data/data";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 
-const Component=styled(Box)`
-display:flex;
-`
-const Caintaner=styled(Box)`
-padding:56px 0 56px 56px
-
-`
-const Title=styled(Typography)`
-font-size:26px;
-color:#525252;
-font-weight:300;
-font-family:inherit;
-margin-bottom:25px;
-
-`
-const Qrcode=styled("img")({
-  height:264,
-  width:264,
-  margin:"50px 0 0 50px"
-
-}
-  
-)
+const Component = styled(Box)`
+  display: flex;
+`;
+const Caintaner = styled(Box)`
+  padding: 56px 0 56px 56px;
+`;
+const Title = styled(Typography)`
+  font-size: 26px;
+  color: #525252;
+  font-weight: 300;
+  font-family: inherit;
+  margin-bottom: 25px;
+`;
+const Qrcode = styled("img")({
+  height: 264,
+  width: 264,
+  margin: "50px 0 0 50px",
+});
+const StyleList = styled(List)`
+  & > li {
+    padding: 0;
+    margin-top: 15px;
+    font-size: 16px;
+    line-height: 28px;
+    color: #747f86;
+  }
+`;
 
 const dailogStyle = {
   height: "96%",
@@ -33,23 +39,42 @@ const dailogStyle = {
   maxWidth: "100%",
   maxHeight: "100%",
   boxShadow: "none",
-  overFlow: "none",
+  overFlow: "hidden",
 };
 const LoginDailog = () => {
+  const LoginSucess = (res) => {
+    const userData=jwt_decode(res.credential)
+    console.log(userData)
+  };
+  const LoginError = (res) => {
+    console.log("login failed",res)
+
+  };
+
+  // google outh run on port localhost:3000
   return (
     <div>
       <Dialog open={true} PaperProps={{ sx: dailogStyle }}>
         <Component>
           <Caintaner>
             <Title>To use whatsapp on your computer</Title>
-            <List>
+            <StyleList>
               <ListItem>1. Open whatsapp on your phone</ListItem>
-              <ListItem>2. Tap menu setting and open select whatsapp web</ListItem>
-              <ListItem>3. Point your phone to this screen to capyure the code</ListItem>
-            </List>
+              <ListItem>
+                2. Tap menu setting and open select whatsapp web
+              </ListItem>
+              <ListItem>3. Tap on Link a Device</ListItem>
+              <ListItem>
+                4. Point your phone to this screen to capyure the code
+              </ListItem>
+            </StyleList>
           </Caintaner>
-          <Box>
-            <Qrcode src={qrCodeImage} alt="qr code"/>
+          <Box style={{position:"relative"}}>
+            <Qrcode src={qrCodeImage} alt="qr code" />
+            <Box style={{position:"absolute" ,top:"50%" ,transform:"translateX(17%)"}}>
+            <GoogleLogin onSuccess={LoginSucess} onError={LoginError} />
+            </Box>
+            
           </Box>
         </Component>
       </Dialog>
