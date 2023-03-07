fd5610d98c81b658119e0862fb9aab83e0bc08b9
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2023-03-07 12:45:35 +0530

    real time chat code

--- Socket/index.js
+++ Socket/index.js
@@ -1,10 +1,34 @@
-const express = require('express')
+import { Server } from "socket.io";
 
-const app = express()
+// to solve  cors error 
+const io=new Server(9000,{
+    cors: {
+        origin :"http://localhost:3000"   
+    }
+})
+// save active user
+let users=[]
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+const adduser=(userData,socketId)=>{
+    !users.some(user=>user.sub==userData.sub) && users.push({...userData,socketId})
 
-app.get('/', (req, res) => res.send('hello'))
+}
 
-app.listen(8080, () => {console.log('server started on port 8080')})
+//
+
+const getuser=(userID)=>{
+    return users.find(user=>user.sub===userID);
+
+}
+io.on("connection",(Socket)=>{
+    console.log("user connection");
+    Socket.on("addUser",userData=>{
+        adduser(userData,Socket.id);
+        io.emit("getuser",users)// send data to fronted user online data useing emit
+    })
+
+    Socket.on("sendMessage",data=>{
+        const user=getuser(data.receverId);
+        io.to(user.socketId).emit("getMessage",data)// 
+    })
+})
